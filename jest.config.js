c387f2e34663b10ba54dfc028207732b7a25f9ee
nkfab <fabjeezy@gmail.com>
2019-03-20 23:09:12 +0200

    chore(setup): setup continuous integration [Finishes #164684533]

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: "node",
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
+  coveragePathIgnorePatterns: [
+    "coverage",
+    "node_modules",
+    "middlewares",
+    "server.js",
+    "app.js",
+    "database",
+    "config"
+  ],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["**/*.js"],
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
+};
